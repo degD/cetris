e13571b595afd24a5110cf7e95d805de938b6579
degD <108623049+degD@users.noreply.github.com>
2022-11-11 18:17:48 +0300

    Wrote header for cetromino_functions.c

--- cetromino_functions.h
+++ cetromino_functions.h
@@ -1,6 +1,28 @@
 
+#include "cetrominobase.h"
+#include "gamedefinitions.h"
+
 #ifndef CETROMINOES_H
 #define CETROMINOES_H
 
+cetrominobase initcetromino(char codename);
+
+float* rotatecoord(float cx, float cy, float x, float y, int dir);
+
+void rotatecetromino(int direction, cetrominobase cetromino);
+
+void descendcetromino(cetrominobase cetromino);
+
+int isoccupied(int x, int y, char grid[ROW][COL]);
+
+int is_cetromino_location_valid(cetrominobase cetromino, char grid[ROW][COL]);
+
+int isatbottom(cetrominobase cetromino, char grid[ROW][COL]);
+
+int is_at_edge(cetrominobase cetromino, char grid[ROW][COL]);
+
+int super_rotation_system(int direction, cetrominobase cetromino, char grid[ROW][COL]);
+
+void add_to_grid(cetrominobase cetromino, int grid[ROW][COL]);
 
 #endif
