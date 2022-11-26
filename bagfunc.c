394899fcb30a3dc634bc9f3a8c87f721893366f0
degD <108623049+degD@users.noreply.github.com>
2022-11-26 15:37:46 +0300

    Added seeding for the bag generation

--- bagfunc.c
+++ bagfunc.c
@@ -1,13 +1,17 @@
 
 #include <stdlib.h>
+#include <time.h>
 #include "gamedefinitions.h"
 
 // Create cetris cetromino bag
 void init_cetris_bag(char cbag[7])
 {
+
     int i, num;
     char selection = ' ';
     char cetromino_list[7] = {'I', 'J', 'L', 'O', 'S', 'T', 'Z'};
+
+    srand(time(NULL));
 
     for (i = 0; i < 7; i++)
     {
